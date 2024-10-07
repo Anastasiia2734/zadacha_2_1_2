15b69fef7148655cba91f5801b7e90c55784bd27
test-github-04 <nastenka.grigoreva@gmail.com>
2024-10-07 16:58:52 +0500

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanOne =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCatOne = (Cat) applicationContext.getBean("cat");
+        Cat beanCatTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanOne.getMessage());
+        System.out.println(beanTwo.getMessage());
+        System.out.println(beanCatOne.getMessageCat());
+        System.out.println(beanCatTwo.getMessageCat());
+        System.out.println(beanCatOne.equals(beanCatTwo));
+        System.out.println(beanOne.equals(beanTwo));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private String messageCat;
+    public Cat() {
+
+    }
+
+    public String getMessageCat() {
+        return messageCat;
+    }
+
+    public void setMessageCat(String messageCat) {
+        this.messageCat = messageCat;
+    }
+}
